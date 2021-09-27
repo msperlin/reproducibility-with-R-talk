04e36588ab01e6c6641c746a933061f1d51472c5
msperlin <marceloperlin@gmail.com>
2021-09-27 09:57:30 -0300

    update

--- 02-R-Code/00-example-base-R-code/script-research.R
+++ 02-R-Code/00-example-base-R-code/script-research.R
@@ -6,14 +6,15 @@ source("fcts/fcts_example.R")
 graphics.off()
 
 df_info <- get_info_companies(cache_folder = tempdir()) |>
-  filter(SIT_REG  == 'ATIVO')
+  dplyr::filter(SIT_REG  == 'ATIVO')
 
 df_plot <- organize_data(df_info)
 
 p <- create_plot(df_plot)
 
-fs::dir_create('/home/docker-output')
-ggsave(filename = '/home/docker-output/plot.png', 
+x11() ; p
+
+ggsave(filename = 'output/plot.png', 
        plot = p, 
        width = 7, height = 7)
 
